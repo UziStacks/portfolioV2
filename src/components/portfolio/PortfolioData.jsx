eaeaf4533ff1609cc3dd10307467b058d7adbb99
Terel <terel.phillips23@gmail.com>
2022-05-20 03:30:48 -0400

    -new portfolio project

--- src/components/portfolio/PortfolioData.jsx
+++ src/components/portfolio/PortfolioData.jsx
@@ -4,6 +4,7 @@ import IMG3 from '../../assets/Images/JsClock.png';
 import IMG4 from '../../assets/Images/portfoliov1.png';
 import IMG5 from '../../assets/Images/todoapp.png';
 import IMG6 from '../../assets/Images/netflixclone.png';
+import IMG7 from '../../assets/Images/ipaddresstracker.png';
 
 export const PortfolioData = [
   {
@@ -15,34 +16,41 @@ export const PortfolioData = [
   },
   {
     id: 2,
+    image: IMG7,
+    title: 'IP Address Tracker',
+    github: 'https://github.com/UziStacks/ip-address-tracker-react',
+    demo: 'https://ip-address-tracker-react-private.vercel.app/',
+  },
+  {
+    id: 3,
     image: IMG5,
     title: 'Todo App',
     github: 'https://github.com/UziStacks/Todo-app',
     demo: 'https://todo-app-gules-mu.vercel.app/',
   },
   {
-    id: 3,
+    id: 4,
     image: IMG1,
     title: 'DevSearch',
     github: 'https://github.com/UziStacks/DevSearch',
     demo: 'https://dev-search.vercel.app',
   },
   {
-    id: 4,
+    id: 5,
     image: IMG2,
     title: 'Countdown LaunchPage',
     github: 'https://github.com/UziStacks/CountdownLaunchPage',
     demo: 'https://countdown-launch-page.vercel.app',
   },
   {
-    id: 5,
+    id: 6,
     image: IMG3,
     title: 'JsClock',
     github: 'https://github.com/UziStacks/JSClock',
     demo: 'https://js-clock-mu.vercel.app/',
   },
   {
-    id: 6,
+    id: 7,
     image: IMG4,
     title: 'First portfolio',
     github: 'https://github.com/UziStacks/devportfoliov1.2',
